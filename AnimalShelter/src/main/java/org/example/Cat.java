e64c951c41e209c68f998497991fa516e215dbcf
oishidemon <orobets.oleh@chnu.edu.ua>
2024-10-16 19:55:22 +0300

    cat goes meow meow

--- AnimalShelter/src/main/java/org/example/Cat.java
+++ AnimalShelter/src/main/java/org/example/Cat.java
@@ -3,6 +3,6 @@ package org.example;
 public class Cat extends Animal {
     @Override
     void makeSound() {
-        //this method is for merge conflict test. everyone should do it differently
+        System.out.println("meow nahuy");
     }
 }
